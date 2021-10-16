3df8d1cfa2cbb3ebf034192fea0ffad669e5910d
ashishsingla9 <92581601+ashishsingla9@users.noreply.github.com>
2021-10-16 15:21:08 +0530

    Update EntryPoint.java

--- src/main/java/com/demo/entrypoint/EntryPoint.java
+++ src/main/java/com/demo/entrypoint/EntryPoint.java
@@ -8,6 +8,7 @@ public class EntryPoint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
 		SpringApplication.run(EntryPoint.class, args);
 	}
 
